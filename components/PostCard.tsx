c60d2f1bf48ebf19523004225db338a9bf57eea8
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-09 15:51:40 +0200

    PostCard component

---
+++ components/PostCard.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import { urlFor } from '../sanity'
+import { Post } from '../typings'
+
+interface post {
+  post: Post
+}
+
+const PostCard = ({ post }: post) => {
+  return (
+    <Link key={post._id} href={`/post/${post.slug.current}`}>
+      <div className="border rounded-lg group cursor-pointer overflow-hidden">
+        <img
+          className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+          src={urlFor(post?.mainImage).url()!}
+          alt="post image"
+        />
+        <div className="flex justify-between p-5 bg-white">
+          <div>
+            <p className="text-lg font-bold">{post.title}</p>
+            <p className="text-sm">
+              {post.description} by {post.author.name}
+            </p>
+          </div>
+          <img
+            className="h-12 w-12 rounded-full ml-4"
+            src={urlFor(post.author.image).url()}
+            alt="author avatar"
+          />
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default PostCard
