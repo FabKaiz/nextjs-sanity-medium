3c7cb130ae98fa9ab1dc2264b7a53d27abc832b4
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-09 15:51:08 +0200

    format

--- sanity.js
+++ sanity.js
@@ -1,8 +1,5 @@
-import {
-  createImageUrlBuilder,
-  createCurrentUserHook,
-  createClient
-} from 'next-sanity'
+import { createCurrentUserHook, createClient } from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
   /**
@@ -12,13 +9,19 @@ export const config = {
    *
    * https://nextjs.org/docs/basic-features/environment-variables
    **/
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: "2021-08-11", // or today's date for latest
+  apiVersion: '2021-08-11', // or today's date for latest
   /**
    * Set useCdn to `false` if your application require the freshest possible
    * data always (potentially slightly slower and a bit more expensive).
    * Authenticated request (like preview) will always bypass the CDN
    **/
-  useCdn: process.env.NODE_ENV === "production",
-};
+  useCdn: process.env.NODE_ENV === 'production',
+}
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+
+export const useCurrentUser = createCurrentUserHook(config)
