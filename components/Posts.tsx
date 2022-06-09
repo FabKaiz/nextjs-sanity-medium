f3656e3d1a8e0fcb97657960325622c552761f2b
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-09 15:51:46 +0200

    Post component

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,6 +1,26 @@
-const Posts = () => {
+import Link from 'next/link'
+import { Post } from '../typings'
+import PostCard from './PostCard'
+
+interface Posts {
+  posts: [Post]
+}
+
+const Posts = ({ posts }: Posts) => {
   return (
-    <div>Posts</div>
+    <>
+      {posts ? (
+        posts.map((post) => (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+            <PostCard post={post} key={post._id} />
+          </div>
+        ))
+      ) : (
+        <h2 className="flex text-center justify-center text-lg mt-6">
+          No post yet
+        </h2>
+      )}
+    </>
   )
 }
 
