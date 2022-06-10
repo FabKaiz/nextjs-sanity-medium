ebe8358e5de9ac9fcf842018f099c21c17e1ce44
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-10 15:16:09 +0200

    handle no post found

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import { Post } from '../typings'
 import PostCard from './PostCard'
 
@@ -9,13 +8,10 @@ interface Posts {
 const Posts = ({ posts }: Posts) => {
   return (
     <>
-      {posts ? (
-        posts.map((post) => (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
-            <PostCard post={post} key={post._id} />
-          </div>
-        ))
-      ) : (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts && posts.map((post) => <PostCard key={post._id} post={post} />)}
+      </div>
+      {!posts && (
         <h2 className="flex text-center justify-center text-lg mt-6">
           No post yet
         </h2>
