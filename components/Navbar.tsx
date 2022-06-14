50c5098d36c2b7b9fc1c28d9166c890478d94433
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-14 10:58:05 +0200

    navbar fixed + changing color on scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,51 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
-const Header = () => {
+const Navbar = () => {
+  const [colorClass, setColorClass] = useState('')
+
+  useEffect(() => {
+    window.addEventListener('scroll', stickNavbar)
+
+    return () => {
+      window.removeEventListener('scroll', stickNavbar)
+    }
+  }, [])
+
+  const stickNavbar = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY
+      console.log(windowHeight)
+      windowHeight > 460 ? setColorClass('bg-white') : setColorClass('')
+    }
+  }
+
   return (
-    <header className="flex justify-between p-3 sm:p-5 max-w-7xl mx-auto">
-      <div className="flex items-center space-x-5">
-        <Link href="/">
-          <img
-            className="w-44 object-containrt cursor-pointer"
-            src="https://links.papareact.com/yvf"
-            alt="medium logo"
-          />
-        </Link>
-        <div className="hidden md:inline-flex items-center space-x-5">
+    <div
+      className={`p-3 top-0 border-b border-black bg-yellow-400 duration-500 transition-colors sm:p-5 w-full flex-row fixed ${colorClass}`}
+    >
+      <div className="flex center mx-auto max-w-7xl items-center justify-between ">
+        <div className="flex items-center space-x-5">
+          <Link href="/">
+            <img
+              className="w-44 object-containrt cursor-pointer"
+              src="https://links.papareact.com/yvf"
+              alt="medium logo"
+            />
+          </Link>
+          <div className="hidden md:inline-flex items-center space-x-5"></div>
+        </div>
+        <div className="flex items-center space-x-2 sm:space-x-5 text-black">
           <h3 className="cursor-pointer">About</h3>
           <h3 className="cursor-pointer">Contact</h3>
-          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full cursor-pointer">
-            Follow
+          <h3 className="cursor-pointer">Sign In</h3>
+          <h3 className="px-2 sm:px-4 py-2 rounded-full bg-black text-white cursor-pointer">
+            Get Started
           </h3>
         </div>
       </div>
-
-      <div className="flex items-center space-x-2 sm:space-x-5 text-green-600 ">
-        <h3 className="cursor-pointer">Sign In</h3>
-        <h3 className="border px-2 sm:px-4 py-1 rounded-full border-green-600 cursor-pointer">
-          Get Started
-        </h3>
-      </div>
-    </header>
+    </div>
   )
 }
 
-export default Header
+export default Navbar
